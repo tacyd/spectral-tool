e37eb441cebd6fe9cd9d36f80ea2cab3a2d8078c
fraserlabmac <fraserlab.mac@gmail.com>
2017-03-09 16:06:27 -0800

    first commit

---
+++ HySP_med_led/HySP_med_led.py
@@ -0,0 +1,231 @@
+import pyfirmata
+import time
+import epix_framegrabber
+from matplotlib import pylab
+import numpy
+#from HySP_med_led import epix_framegrabber
+
+try:
+    epix_framegrabber.Camera()
+    epix_available = True
+except epix_framegrabber.CameraOpenError:
+    epix_available = False
+
+
+
+
+#open the camera
+camera = epix_framegrabber.Camera()
+aa = camera.open(8, [2048, 1088],camera = "PhotonFocus",exposure = 10,frametime = 100.0)
+
+for i in range(0,3):
+    
+    camera.set_exposure(10000*(10*i))    
+    #aa = camera.start_sequence_capture(1)
+
+    bb= camera.start_sequence_capture(1)
+    cc = camera.get_image()
+
+    #bb = numpy.array(aa)
+
+
+
+
+    #camera.close()
+    pylab.imshow(cc)
+    pylab.show()
+
+
+camera.close()
+#try:
+#    board = pyfirmata.ArduinoDue('\\.\COM6')
+#except Exception:
+#    print('error')
+
+#for i in range(23,30):
+    
+#    #remember to put PIN down!! only one LED open at the time
+#    board.digital[i].write(1)
+#    time.sleep(.5)
+#    board.digital[i].write(0)
+
+
+
+    
+
+
+
+
+
+#"""
+#This recipe opens a simple window in PyQt to poll the serial port for 
+#data and print it out. Uses threads and a queue.
+
+#DON'T FORGET TO SET SERIALPORT BEFORE RUNNING THIS CODE
+
+#Here is an Arduino Sketch to use:
+#    ***********
+#void setup() {
+#  Serial.begin(115200);
+#}
+#void loop() {
+#  Serial.println("I blinked");
+#  delay(1000);
+#}
+#    ***********
+#This recipe depends on PyQt and pySerial. Qt 4 is world class code 
+#and I like how it looks. PyQt is not completely open source, 
+#but I think PySide is. Tested on Qt4.6 / Win 7 / Duemilanove
+
+#Author: Dirk Swart, Ithaca, NY. 2011-05-20. www.wickeddevice.com
+
+#Based on threads recipe by Jacob Hallen, AB Strakt, Sweden. 2001-10-17
+#As adapted by Boudewijn Rempt, Netherlands. 2002-04-15
+
+#PS: This code is provided with no warranty, express or implied. It is 
+#meant to demonstrate a concept only, not for actual use. 
+#Code is in the public domain.
+#"""
+#__author__ = 'Dirk Swart, Doudewijn Rempt, Jacob Hallen'
+
+#import sys, time, threading, random, Queue
+#from PyQt5 import QtGui, QtCore as qt
+#import serial
+
+#SERIALPORT = 'COM6'
+
+#class GuiPart(QtGui.QMainWindow):
+
+#    def __init__(self, queue, endcommand, *args):
+#        QtGui.QMainWindow.__init__(self, *args)
+#        self.setWindowTitle('Arduino Serial Demo')
+#        self.queue = queue
+#        # We show the result of the thread in the gui, instead of the console
+#        self.editor = QtGui.QTextEdit(self)
+#        self.setCentralWidget(self.editor)
+#        self.endcommand = endcommand    
+        
+#    def closeEvent(self, ev):
+#        self.endcommand()
+
+#    def processIncoming(self):
+#        """
+#        Handle all the messages currently in the queue (if any).
+#        """
+#        while self.queue.qsize():
+#            try:
+#                msg = self.queue.get(0)
+#                # Check contents of message and do what it says
+#                # As a test, we simply print it
+#                self.editor.insertPlainText(str(msg))
+#            except Queue.Empty:
+#                pass
+
+#class ThreadedClient:
+#    """
+#    Launch the main part of the GUI and the worker thread. periodicCall and
+#    endApplication could reside in the GUI part, but putting them here
+#    means that you have all the thread controls in a single place.
+#    """
+#    def __init__(self):
+#        # Create the queue
+#        self.queue = Queue.Queue()
+
+#        # Set up the GUI part
+#        self.gui=GuiPart(self.queue, self.endApplication)
+#        self.gui.show()
+
+#        # A timer to periodically call periodicCall :-)
+#        self.timer = qt.QTimer()
+#        qt.QObject.connect(self.timer,
+#                           qt.SIGNAL("timeout()"),
+#                           self.periodicCall)
+#        # Start the timer -- this replaces the initial call to periodicCall
+#        self.timer.start(100)
+
+#        # Set up the thread to do asynchronous I/O
+#        # More can be made if necessary
+#        self.running = 1
+#        self.thread1 = threading.Thread(target=self.workerThread1)
+#        self.thread1.start()
+
+#    def periodicCall(self):
+#        """
+#        Check every 100 ms if there is something new in the queue.
+#        """
+#        self.gui.processIncoming()
+#        if not self.running:
+#            root.quit()
+
+#    def endApplication(self):
+#        self.running = 0
+
+#    def workerThread1(self):
+#        """
+#        This is where we handle the asynchronous I/O. 
+#        Put your stuff here.
+#        """
+#        while self.running:
+#            #This is where we poll the Serial port. 
+#            #time.sleep(rand.random() * 0.3)
+#            #msg = rand.random()
+#            #self.queue.put(msg)
+#            ser = serial.Serial(SERIALPORT, 115200)
+#            msg = ser.readline();
+#            if (msg):
+#                self.queue.put(msg)
+#            else: pass  
+#            ser.close()
+
+##rand = random.Random()
+#root = QtGui.QApplication(sys.argv)
+#client = ThreadedClient()
+#sys.exit(root.exec_())
+
+##import sys
+##import glob
+##import serial
+
+
+##def serial_ports():
+##    """ Lists serial port names
+
+##        :raises EnvironmentError:
+##            On unsupported or unknown platforms
+##        :returns:
+##            A list of the serial ports available on the system
+##    """
+##    if sys.platform.startswith('win'):
+##        ports = ['COM%s' % (i + 1) for i in range(256)]
+##    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+##        # this excludes your current terminal "/dev/tty"
+##        ports = glob.glob('/dev/tty[A-Za-z]*')
+##    elif sys.platform.startswith('darwin'):
+##        ports = glob.glob('/dev/tty.*')
+##    else:
+##        raise EnvironmentError('Unsupported platform')
+
+##    result = []
+##    for port in ports:
+##        try:
+##            s = serial.Serial(port)
+##            s.close()
+##            result.append(port)
+##        except (OSError, serial.SerialException):
+##            pass
+##    return result
+
+
+##if __name__ == '__main__':
+##    print(serial_ports())
+
+##def main():
+
+##    app = QtGui.QApplication(sys.argv)
+##    ex = captureFrames()
+##    app.aboutToQuit.connect(ex.closing_sequence)
+##    sys.exit(app.exec_())
+
+
+##if __name__ == '__main__':
+##    main()
---
+++ HySP_med_led/epix_framegrabber.py
@@ -0,0 +1,292 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright 2014, Thomas G. Dimiduk, Rebecca W. Perry, Aaron Goldfain
+#
+# This file is part of Camera Controller
+#
+# Camera Controller is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# HoloPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with HoloPy.  If not, see <http://www.gnu.org/licenses/>.
+"""
+Higher Level interface to the epix framegrabber
+
+.. moduleauthor:: Thomas G. Dimiduk <tom@dimiduk.net>
+.. moduleauthor:: Rebecca W. Perry <perry.becca@gmail.com>
+"""
+import numpy as np
+import os.path
+import os
+import pypylon
+
+#epix = windll.LoadLibrary("C:\Program Files\EPIX\XCLIB\clserEPX_w64.dll")
+
+class CameraOpenError(Exception):
+    def __init__(self, mesg):
+        self.mesg = mesg
+    def __str__(self):
+        return self.mesg
+
+class Camera(object):
+    def __init__(self):
+        self.pixci_opened = False
+        self.bit_depth = None
+        self.roi_shape = None
+        self.camera = None
+        self.exposure = None
+        self.roi_pos = None
+        self.frametime = None
+        try:
+            from ctypes import c_ubyte, windll, sizeof, c_ushort
+        except ImportError:
+            raise CameraOpenError("Import failed")
+        #print('hello')
+        self.epix = windll.LoadLibrary("C:\Program Files\EPIX\XCLIB\XCLIBW64.dll")
+        #self.pylon = windll.LoadLibrary
+
+    def open(self, bit_depth=8, roi_shape=(2048, 1088), roi_pos = (0,0), camera=None, exposure = 0.1, frametime = 0):
+#    def open(self, bit_depth=10, roi_shape=(500, 500), roi_pos = (0,0), camera=None, exposure = 0, frametime = 0):	
+#    def open(self, bit_depth=10, roi_shape=(2048, 1088), roi_pos = (0,0), camera=Basler, exposure = 0, frametime = 0):		
+        if self.pixci_opened:
+            self.close()
+        print('this is open camera in epixframegrabber')
+        from ctypes import c_ubyte, windll, sizeof, c_ushort
+
+        self.bit_depth = bit_depth
+        self.roi_shape = roi_shape
+        self.camera = camera
+        self.roi_pos = roi_pos
+        self.exposure = exposure
+        self.frametime = frametime
+
+        filename = "{0}_{1}bit_{2}x{3}.fmt".format(self.camera,self.bit_depth,*self.roi_shape)
+        formatfile = os.path.join("formatFiles", filename)
+        
+        #i = self.epix.pxd_PIXCIopen("","", b"OurCamera.fmt") # standard NTSC
+        i = self.epix.pxd_PIXCIopen("","", b"PhotonFocus_8bit_1024x1024.fmt") # standard NTSC
+        #i = self.epix.pxd_PIXCIopen("","", filename.encode(encoding='UTF-8')) # standard NTSC
+        #i = self.epix.pxd_PIXCIopen("","", "") # standard NTSC
+        test = self.epix.pxd_mesgErrorCode(i)
+        test2 = self.epix.pxd_mesgFault(1)
+        if i == 0:
+            print("Frame grabber opened successfully.")
+            self.pixci_opened = True
+        elif i == -13:
+            raise CameraOpenError("Frame grabber can't find format file.")
+        elif i == -23:
+            raise CameraOpenError("Frame grabber is already open.")
+        else:
+            raise CameraOpenError("Opening the frame grabber failed with error code "+str(i))
+            #epix.pxd_mesgFault(1)
+        
+        #Use pypylon to set parameters on camera
+
+        #search camera
+        available_cameras = pypylon.factory.find_devices()
+        print('Available cameras are', available_cameras)
+        self.cam = pypylon.factory.create_device(available_cameras[-1])
+
+        # We can still get information of the camera back
+        print('Camera info of camera object:', self.cam.device_info)
+
+        # Open camera and grep some images
+        self.cam.open()
+
+        # Hard code exposure time
+        self.cam.properties['ExposureTimeAbs'] = 15000.0 #exposure time in microseconds
+        
+        # Set GainRaw
+        self.cam.properties['GainRaw'] = 350 #max should be 350, check manual
+
+
+        #list of properties of camera (check here for how to change parameters on camera)
+        for key in self.cam.properties.keys():
+            try:
+                value = self.cam.properties[key]
+            except IOError:
+                value = '<NOT READABLE>'
+
+            print('{0} ({1}):\t{2}'.format(key, self.cam.properties.get_description(key), value))
+
+
+
+        #initialize serial port
+        #r = self.epix.pxd_serialConfigure(0x1, 0,  9600, 8, 0, 1, 0, 0, 0)
+        #if (r < 0):
+        #    print('error')
+        #command = 'DeviceModelName'
+        ##command.encode()
+        #test11 = self.epix.pxd_serialWrite(0x1, 0, command.encode())
+        #test22 = self.epix.pxd_serialRead(0x1, 0)
+        #buff_char =  "\0" * 100
+        #buff_char.encode()
+        #test22 = self.epix.pxd_serialRead(0x1, 0, buff_char)
+
+        #command = 'GainAll = 128'
+        #test23 =self.epix.pxd_serialWrite(0x1,0,command.encode())
+
+        aa = 00
+ 
+
+        
+    def close(self):
+        if self.open:
+            self.cam.close()
+            print('Camera closed successfully')
+            i = self.epix.pxd_PIXCIclose("",b"NTSC","") # standard NTSC #-25
+            if i == 0:
+                
+                print("Frame grabber closed successfully.")
+                self.open = True
+            elif i == -25:
+                print("Failed to close the frame grabber because it wasn't open.")
+            else:
+                print("Closing the frame grabber failed with error code "+str(i))
+        else:
+            return
+
+    def get_image(self, buffer_number=None):
+        try:
+            from ctypes import c_ubyte, windll, sizeof, c_ushort
+        except ImportError:
+            raise CameraOpenError("Import failed")
+
+        if buffer_number is None:
+            buffer_number = self.epix.pxd_capturedBuffer(1)
+        # TODO: can we use the locally stored values for these? Or is
+        # there some subtle way that will lead us astray?
+        xdim = self.epix.pxd_imageXdim()
+        ydim = self.epix.pxd_imageYdim()
+
+        imagesize = xdim*ydim
+
+        if self.bit_depth > 8:
+            c_type = c_ushort
+            cam_read = self.epix.pxd_readushort
+        else:
+            c_type = c_ubyte
+            cam_read = self.epix.pxd_readuchar
+        c_buf = (c_type * imagesize)(0)
+        c_buf_size = sizeof(c_buf)
+
+        cam_read(0x1, buffer_number, 0, 0, -1, ydim, c_buf,
+                           c_buf_size, b"Gray")
+
+        im = np.frombuffer(c_buf, c_type).reshape([xdim, ydim])
+        #imshow
+        
+        if self.bit_depth > 8:
+            # We have to return a 16 bit image, use the upper bits so that
+            # outputs look nicer (max pixel intensity will be interpreted as
+            # white by image viewers). i.e. linearly rescale pixel values to
+            # a 16 bit scale : 0 to 65535. Note 65535 = (2**16 - 1).
+            im = im * 2**(16-self.bit_depth)
+
+        return im
+
+    def set_gain(self, gain_value):
+        # Set GainRaw
+        if gain_value>350:
+            gain_value = 350
+        if gain_value< 0:
+            gain_value = 1
+
+        self.cam.properties['GainRaw'] = gain_value #max should be 350, check manual
+
+    def set_exposure(self,exposure_value):
+        #exposure in microseconds
+        if exposure_value<10862:
+            exposure_value = 10862 #min value
+        self.cam.properties['ExposureTimeAbs'] = exposure_value
+
+
+    def get_image_new(self, buffer_number=None):
+        try:
+            from ctypes import c_ubyte, windll, sizeof, c_ushort
+        except ImportError:
+            raise CameraOpenError("Import failed")
+
+        if buffer_number is None:
+            buffer_number = self.epix.pxd_capturedBuffer(1)
+        # TODO: can we use the locally stored values for these? Or is
+        # there some subtle way that will lead us astray?
+        xdim = self.epix.pxd_imageXdim()
+        ydim = self.epix.pxd_imageYdim()
+
+        imagesize = xdim*ydim
+        #note this edit
+        
+        self.bit_depth = 8
+        
+        if self.bit_depth > 8:
+            c_type = c_ushort
+            cam_read = self.epix.pxd_readushort
+        else:
+            c_type = c_ubyte
+            cam_read = self.epix.pxd_readuchar
+        c_buf = (c_type * imagesize)(0)
+        c_buf_size = sizeof(c_buf)
+
+        cam_read(0x1, buffer_number, 0, 0, -1, ydim, c_buf,
+                           c_buf_size, b"Gray")
+
+        im = np.frombuffer(c_buf, c_type).reshape([xdim, ydim])
+        #imshow
+        
+        if self.bit_depth > 8:
+            # We have to return a 16 bit image, use the upper bits so that
+            # outputs look nicer (max pixel intensity will be interpreted as
+            # white by image viewers). i.e. linearly rescale pixel values to
+            # a 16 bit scale : 0 to 65535. Note 65535 = (2**16 - 1).
+            im = im * 2**(16-self.bit_depth)
+
+        return im
+
+    def get_frame_number(self):
+        return self.epix.pxd_capturedBuffer(0x1,1)-1
+
+    def finished_live_sequence(self):
+        return self.epix.pxd_goneLive(0x1) == 0
+
+    def start_continuous_capture(self, buffersize):
+        '''
+        buffersize: number of frames to keep in rolling buffer
+        '''
+        # This appears to give an infinitely long live seqence, however it
+        # looks like it may do it by continually overwriting the same image in
+        # the buffer, so it probably will not work if we want a rolling buffer
+        # tgd 2014-06-16
+        # you can get the same effect by changing 1000000 to 0
+        #self.epix.pxd_goLive(0x1,1)
+        self.epix.pxd_goLiveSeq(0x1,1,buffersize,1,1000000,1)
+
+    def start_sequence_capture(self, n_frames):
+        print('sequence capture started')
+        self.epix.pxd_goLiveSeq(0x1,1,n_frames,1,n_frames,1)
+
+    def stop_live_capture(self, ):
+        print('unlive now')
+        self.epix.pxd_goUnLive(0x1)
+        
+        
+    def set_roi_pos(self, set_roi_pos):
+        #not yet implemented
+        self.roi_pos = [0, 0]
+    
+    def set_frametime(self, frametime):
+        #not yet implemented
+        self.frametime = 0
+
+    def get_snapshot(self):
+        self.epix.pxd_goSnap(0x1,1)
+
+
